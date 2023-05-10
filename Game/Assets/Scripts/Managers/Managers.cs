d35df719b7bf71684aacd6ccd37a1048819e760b
Jiwon Choi <sjndlw1013@gmail.com>
2023-05-10 14:51:20 +0900

    Managers 추가, 배경 음악 설정

--- Game/Assets/Scripts/Managers/Managers.cs
+++ Game/Assets/Scripts/Managers/Managers.cs
@@ -2,14 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Managers : MonoBehaviour
 {
     static Managers s_instance;
     static Managers Instance { get { Init(); return s_instance; } }
 
+    DataManager _data  = new DataManager();
+    FieldManager _field = new FieldManager();
     InputManager _input = new InputManager();
+    InventoryManager _inventory = new InventoryManager();
+    ResourceManager _resource = new ResourceManager();
+    SoundManager _sound = new SoundManager();
+    TimeManager _time = new TimeManager();
+    UIManager _ui = new UIManager();
+    
+
+    public static DataManager Data { get { return Instance._data; } }
+    public static FieldManager Field { get { return Instance._field; } }
     public static InputManager Input { get { return Instance._input; } }
+    public static InventoryManager Inventory { get { return Instance._inventory; } }
+    public static ResourceManager Resource { get { return Instance._resource; } }
+    public static SoundManager Sound { get { return Instance._sound; } }
+    public static TimeManager Time { get { return Instance._time; } }
+    public static UIManager UI { get { return Instance._ui; } }
+    
 
 
     void Start()
