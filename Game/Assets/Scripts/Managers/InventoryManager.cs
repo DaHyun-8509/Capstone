61f343e44433159272f97d519d9643d4a7af34f8
Jiwon Choi <sjndlw1013@gmail.com>
2023-05-20 19:48:34 +0900

    Manager들 버그 수정 (상속관계 수정)

--- Game/Assets/Scripts/Managers/InventoryManager.cs
+++ Game/Assets/Scripts/Managers/InventoryManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InventoryManager : MonoBehaviour
+public class InventoryManager
 {
     // Start is called before the first frame update
     void Start()
--- Game/Assets/Scripts/Managers/TimeManager.cs
+++ Game/Assets/Scripts/Managers/TimeManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TimeManager : MonoBehaviour
+public class TimeManager
 {
     // Start is called before the first frame update
     void Start()
