20d95b6edd29608ae491480ac51263cb45db8ce7
Jiwon Choi <sjndlw1013@gmail.com>
2023-07-04 11:41:17 +0900

    William 루틴 완성

--- Game/Assets/AIMove_William.cs
+++ Game/Assets/AIMove_William.cs
@@ -4,24 +4,26 @@ using UnityEngine;
 using UnityEngine.AI;
 using static UnityEngine.GraphicsBuffer;
 using TMPro;
-
+using Unity.VisualScripting;
 
 public class AIMove_William : MonoBehaviour
 {
     private NavMeshAgent navMeshAgent;
     public Transform bench;
-    public Transform workplace1;
+    public Transform home;
+    public Transform restuarant;
+
+    public Transform[] workplace;
     Animator anim;
+
+    [SerializeField]
     William_Location location = William_Location.Home;
     bool ismoving = false;
-    bool onDestination = false;
 
     enum William_Location
     {
         Home,
-        Workplace1,
-        Workplace2,
-        Workplace3,
+        Workplace,
         Bench,
         Restaurant
     }
@@ -39,53 +41,94 @@ public class AIMove_William : MonoBehaviour
             //정해진 시간에 목적지 할당
             switch (Managers.Time.GetHour())
             {
-                case 1:
+                case 7:
+                    {
+                        MoveToLocation(workplace[0].position);
+                        location = William_Location.Workplace;
+                    }
+                    break;
+                case 8:
+                    {
+                        MoveToLocation(workplace[1].position);
+                        location = William_Location.Workplace;
+                    }
+                    break;
+                case 9:
+                    {
+                        MoveToLocation(workplace[2].position);
+                        location = William_Location.Workplace;
+                    }
+                    break;
+                case 10:
+                    {
+                        MoveToLocation(workplace[3].position);
+                        location = William_Location.Workplace;
+                    }
+                    break;
+                case 12:
                     {
                         MoveToLocation(bench.position);
                         location = William_Location.Bench;
                     }
                     break;
-                case 3:
+                case 15:
                     {
-                        MoveToLocation(workplace1.position);
-                        location = William_Location.Workplace1;
+                        MoveToLocation(workplace[0].position);
+                        location = William_Location.Workplace;
                     }
                     break;
-                case 10:
+
+                case 18:
+                    {
+                        MoveToLocation(restuarant.position);
+                        location = William_Location.Restaurant;
+                    }
                     break;
-                case 14:
+                case 21:
+                    { 
+                        MoveToLocation(home.position);
+                        location = William_Location.Home;
+                    }
                     break;
                 default:
                     break;
             }
         }
-        
-        //도착했을 때 
-        if(ismoving&& navMeshAgent.hasPath && navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+
+        if (ismoving && navMeshAgent.hasPath && navMeshAgent.remainingDistance  <= navMeshAgent.stoppingDistance)
         {
             ismoving = false;
-            onDestination = true;
+           
             switch (location)
             {
                 case William_Location.Home:
+                    {
+                    }
                     break;
                 case William_Location.Bench:
-                    SitOnTheBench();
+                    {
+                        SitOnTheBench();
+                    }
                     break;
-                case William_Location.Workplace1:
-                    Work();
+                case William_Location.Workplace:
+                    {
+                        InvokeRepeating("Work", 0f, 2f);
+                    }
                     break;
-                case William_Location.Workplace2:
-                    Work();
+                case William_Location.Restaurant:
+                    {
+                        SitInRestaurant();
+                    }
                     break;
                 default:
                     break;
             }
-        }
+        }  
     }
 
     private void MoveToLocation(Vector3 location )
     {
+        CancelInvoke("Work");
         navMeshAgent.destination = location;
         anim.SetFloat("move_speed", 2);
         ismoving = true;
@@ -104,4 +147,14 @@ public class AIMove_William : MonoBehaviour
         anim.SetFloat("move_speed", 0);
         anim.SetTrigger("pick_fruit");
     }
+
+    private void SitInRestaurant()
+    {
+        //Quaternion targetRotation = Quaternion.LookRotation(-Vector3.forward);
+        //transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 10 * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(0, 200, 0);
+        anim.SetFloat("move_speed", 0);
+        anim.SetTrigger("sit");
+    }
+
 }
