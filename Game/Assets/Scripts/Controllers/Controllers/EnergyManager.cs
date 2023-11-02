8e80dcbc02c92c47faff2c8e0aeb043ec0b612a0
Jiwon Choi <sjndlw1013@gmail.com>
2023-11-02 11:08:36 +0900

    에너지 다쓰면 잠들고 리스폰

--- Game/Assets/Scripts/Controllers/Controllers/EnergyManager.cs
+++ Game/Assets/Scripts/Controllers/Controllers/EnergyManager.cs
@@ -48,4 +48,9 @@ public class EnergyManager
             curEnergy = 0;
         }
     }
+
+    public float GetEnergy()
+    {
+        return curEnergy;
+    }
 }
---
+++ Game/Assets/ZeroEnergy.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZeroEnergy : MonoBehaviour
+{
+    [SerializeField] GameObject sleepUI;
+    [SerializeField] Transform spawnPos;
+
+    int sleepStartTime;
+
+    void Update()
+    {
+        if(Managers.Energy.GetEnergy() <= 0.0001)
+        {
+            Managers.Energy.IncreaseEnergy(100);
+            sleepUI.SetActive(true);
+            transform.position = spawnPos.position;
+            sleepStartTime = Managers.Time.GetHour();
+            Managers.Time.NowSpeed = 30f;
+            StartCoroutine(TimeRecover());
+        }
+    }
+
+    IEnumerator TimeRecover()
+    {
+        int sleepEndTime = sleepStartTime + 10;
+        if (sleepEndTime >= 24)
+            sleepEndTime -= 24;
+        yield return new WaitUntil(() => Managers.Time.GetHour() == sleepEndTime );
+        Managers.Time.NowSpeed = 1;
+        sleepUI.SetActive(false);
+
+    }
+}
